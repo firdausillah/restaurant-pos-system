b5c4c44982542ebc82a6aaa42719926528419539
firdausillah <firdausillah123@gmail.com>
2024-04-14 23:00:11 +0700

    update ignore

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-      typescript: {
+module.exports = {
+  typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     // !! WARN !!
     ignoreBuildErrors: true,
   },
-};
-
-export default nextConfig;
+}
