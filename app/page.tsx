574fd1d4a1f4be57933e3e7c112458d70a0c01d2
firdausillah <firdausillah123@gmail.com>
2024-04-22 12:24:14 +0700

    update redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Content from "./components/Content";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <>
-      <Navbar/>
-      <main className="h-screen w-full absolute left-0 top-0 overflow-hide bg-white pt-[113px] md:pl-[80px] md:pt-[70px] ">
-        <Content/>
-      </main>
-    </>
-  );
+  redirect('order');
 }
